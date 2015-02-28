14cdaf70e81a2652d2651bc90dfd972ad75bca08
venubabukolla <vk423@mail.umkc.edu>
2015-02-27 22:06:03 -0600

    Project Phase-I

---
+++ venubabukolla_Scientists-master/src/com/ibm/scientists/Config.java
@@ -0,0 +1,23 @@
+package com.ibm.scientists;
+
+//import com.ibm.personafusion.db.CloudantClient;
+import com.ibm.scientists.db.CloudantDatabase;
+
+public class Config 
+{
+    
+    /** Cloudant **/
+    public static final int CLOUDANT_PORT = 443;
+    public static final String CLOUDANT_NAME = "talent-manager";
+    
+    /** Watson User Modeling **/
+    
+    public static final String WATSON_PROF_API = "/api/v2/profile";
+    public static final String WATSON_VIZ_API = "/api/v2/visualize";
+    
+    public static CloudantDatabase cc = new CloudantDatabase();
+
+    /* Mobile Data Config */
+    public static final String MOBILE_DATA_APP_ID = "";
+    public static final String MOBILE_DATA_APP_SECRET = "";
+}
